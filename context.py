78fc1a40f8e7195aaea002380c3ffb3c5868f5b3
Mizogg <88630056+Mizogg@users.noreply.github.com>
2023-07-08 23:22:23 +0100

    Update context.py

--- context.py
+++ context.py
@@ -2,12 +2,7 @@ fShutdown = False
 listfThreadRunning = [False] * 2 
 local_height = 0 
 nHeightDiff = {}
-
-
 updatedPrevHash = None
-
-
-
 job_id = None 
 prevhash = None 
 coinb1 = None 
@@ -17,11 +12,8 @@ version = None
 nbits = None 
 ntime = None 
 clean_jobs = None 
-
 sub_details = None 
 extranonce1 = None 
 extranonce2 = None
 extranonce2_size = None
-
-
 sock = None
